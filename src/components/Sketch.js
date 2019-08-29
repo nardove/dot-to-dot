e24968bf22f34244046a0138b591b7156d014bf9
nardove <nardove@yahoo.com>
2019-08-29 19:17:10 +0100

    added a better matterjs example

--- src/components/Sketch.js
+++ src/components/Sketch.js
@@ -3,35 +3,36 @@ import Matter from 'matter-js';
 
 
 export default function Sketch() {
-	let Engine, World, Bodies, Render;
-	let box, boxA, boxB;
-	let posX = 200,
-		posY = 140,
-		sizeW = 80,
-		sizeH = 40;
+	let engine, Engine, World, Bodies, Render;
 	
 	window.onload = function() {
 		paper.install(window);
 		paper.setup('paper-canvas');
 		
 		let size = view.size;
-		console.log(size);
 
 		Engine = Matter.Engine;
 		World = Matter.World;
 		Bodies = Matter.Bodies;
 		Render = Matter.Render;
-		
-		let	engine = Engine.create();
+		engine = Engine.create();
 
-		boxA = Bodies.rectangle(posX, posY, sizeW, sizeH);
-		boxA.angle = 0;
+		for (let i=0; i < 10; i++) {
+			let posX = size.width / 2 + Math.random() * 400 - 200,
+				posY = Math.random() * 100,
+				sizeW = Math.random() * 80 + 20,
+				sizeH = Math.random() * 80 + 20,
+				radius = Math.random() * 80 + 15;
 
-		boxB = Bodies.rectangle(posX + 40, size.height - 10 - sizeH, sizeW, sizeH);
-
-		let ground = Bodies.rectangle(size.width / 2, size.height - 10, size.width, 100, {isStatic: true});
-		
-		World.add(engine.world, [boxA, boxB, ground]);
+			addBox(posX, posY, sizeW, sizeH, false);
+			addCircle(posX, posY, radius, false);
+		}
+		// ground
+		addBox(size.width / 2, size.height - 10, size.width, 100, true);
+		// left wall
+		addBox(25, size.height / 2, 50, size.height, true);
+		// right wall
+		addBox(size.width - 25, size.height / 2, 50, size.height, true);
 
 		let showRender = true;
 		if (!showRender) {
@@ -43,28 +44,52 @@ export default function Sketch() {
 			render.canvas.width = size.width;
 			render.canvas.height = size.height;
 			Render.run(render);
+
+			let bounds = new Shape.Rectangle({
+				point: [0, 0],
+				size: [size.width, size.height],
+				strokeColor: 'grey',
+				strokeWidth: 10
+			});
 		}
-
-		box = new Shape.Rectangle({
-			point: [posX, posY],
-			size: [sizeW, sizeH],
-			fillColor: 'pink'
-		});
-
-		let bounds = new Shape.Rectangle({
-			point: [0, 0],
-			size: [size.width, size.height],
-			strokeColor: 'red',
-			strokeWidth: 10
-		});
 
 		view.onFrame = draw;
 		Engine.run(engine);
 	}
 
 	function draw(event) {
-		box.position = boxA.position;
-		box.rotation = boxA.angle * 180 / Math.PI;
+		const bodies = engine.world.bodies;
+		for (let i = 0; i < bodies.length; i++) {
+			var body = bodies[i];
+			var shape = body.shape;
+			shape.position = body.position;
+			shape.rotation =  body.angle * 180 / Math.PI;
+		}
+	}
+
+	function attachBodyShape(body, shape) {
+		body.shape = shape;
+		World.add(engine.world, body);
+	}
+
+	function addCircle(x, y, r, isStatic) {
+		const shape = new Shape.Circle({
+			point: [x, y],
+			radius: r,
+			fillColor: Color.random()
+		});
+		const body = Bodies.circle(x, y, r, Object.assign({isStatic: isStatic}));
+		attachBodyShape(body, shape);
+	}
+
+	function addBox(x, y, w, h, isStatic) {
+		const shape = new Shape.Rectangle({
+											point: [x, y],
+											size: [w, h],
+											fillColor: (isStatic) ? 'grey' : Color.random()
+										});
+		const body = Bodies.rectangle(x, y, w, h, Object.assign({isStatic: isStatic}));
+		attachBodyShape(body, shape);
 	}
 
 	return null;
